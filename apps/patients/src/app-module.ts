8fb08988c915d91495747e420723c9c3116ee920
Key <mkeiver@uninorte.edu.co>
2025-09-29 23:28:26 -0500

    Patients Module (Local Ver)
    patients module can run in local

--- apps/patients/src/app-module.ts
+++ apps/patients/src/app-module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PatientsModule } from './presentation/patients.module';
+import { join } from 'path';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ 
+      isGlobal: true,
+      envFilePath: join(__dirname, '..', '.env')
+     }),
     PatientsModule,
   ],
 })
